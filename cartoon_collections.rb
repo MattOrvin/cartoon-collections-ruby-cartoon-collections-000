8444b7dc813b122154a4d63cbc2d376f016d0475
Matt Orvin <matthew.orvin@gmail.com>
2019-04-01 23:08:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,15 @@ def roll_call_dwarves(dwarves)
 end
 end
 
+def roll_call_dwarves_half(dwarves)
+  roll_call = dwarves.length / 2
+  counter = 0
+  while counter < roll_call
+  puts "#{dwarves[counter]}"
+  counter += 1
+end
+end
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |elements|
   elements.capitalize << "!"
@@ -17,8 +26,18 @@ end
 end
 
 def find_the_cheese(ingredients)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert", "swiss"]
   ingredients.find do |items|
     cheese_types.include?(items)
 end
 end
+
+def words_that_start_with_b(words)
+  words.select do |b_word|
+    b_word.starts_with?("b") 
+end
+end
+
+words_that_start_with_b(["brian", "joe", "james"]) #=> ["brian"]
+words_that_start_with_b(["bob", "bill", "eric"]) #=> ["bob", "bill"]
+words_that_start_with_b(["eric", "prince", "matt"]) #=> []
